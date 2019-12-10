448c02c24b1b46530945330ad78ebbf664ab2117
HITalexwang <hitalexwang@gmail.com>
2019-12-10 15:56:22 -0800

    add easyfirst parser, need to test

--- neuronlp2/io/utils.py
+++ neuronlp2/io/utils.py
@@ -141,9 +141,128 @@ def iterate_bucketed_batch(data, batch_size, unk_replace=0., shuffle=False):
             batch.update({key: field[excerpt, :2 * batch_length - 1] for key, field in data.items() if key in stack_keys})
             yield batch
 
+def iterate_bucketed_batch_and_sample(data, batch_size, unk_replace=0., shuffle=False, max_layers=4):
+    data_tensor, bucket_sizes = data
+
+    bucket_indices = np.arange(len(bucket_sizes))
+    if shuffle:
+        np.random.shuffle((bucket_indices))
+
+    stack_keys = ['STACK_HEAD', 'CHILD', 'SIBLING', 'STACK_TYPE', 'SKIP_CONNECT', 'MASK_DEC']
+    exclude_keys = set(['SINGLE', 'WORD', 'LENGTH'] + stack_keys)
+    easyfirst_keys = ['WORD', 'LENGTH', 'POS', 'CHAR', 'HEAD', 'TYPE']
+    stack_keys = set(stack_keys)
+    for bucket_id in bucket_indices:
+        data = data_tensor[bucket_id]
+        bucket_size = bucket_sizes[bucket_id]
+        if bucket_size == 0:
+            continue
+
+        words = data['WORD']
+        single = data['SINGLE']
+        bucket_length = words.size(1)
+        if unk_replace:
+            ones = single.new_ones(bucket_size, bucket_length)
+            noise = single.new_empty(bucket_size, bucket_length).bernoulli_(unk_replace).long()
+            words = words * (ones - single * noise)
+
+        indices = None
+        if shuffle:
+            indices = torch.randperm(bucket_size).long()
+            indices = indices.to(words.device)
+        for start_idx in range(0, bucket_size, batch_size):
+            if shuffle:
+                excerpt = indices[start_idx:start_idx + batch_size]
+            else:
+                excerpt = slice(start_idx, start_idx + batch_size)
+
+            lengths = data['LENGTH'][excerpt]
+            batch_length = lengths.max().item()
+            # [batch_size, batch_len]
+            heads = data['HEAD'][excerpt, :batch_length]
+            types = data['TYPE'][excerpt, :batch_length]
+            batch = {'WORD': words[excerpt, :batch_length], 'LENGTH': lengths}
+            batch.update({key: field[excerpt, :batch_length] for key, field in data.items() if key in easyfirst_keys})
+            batch_by_layer = sample_generate_order(batch, lengths, n_recomp=max_layers-1)
+            yield batch_by_layer
+
+def sample_generate_order(batch, lengths, n_recomp=3):
+
+    RECOMP = -1
+    EOS = -2
+    NO_RECOMP = 0
+    DO_RECOMP = 1
+    DO_EOS = 2
+    batch_length = lengths.max().item()
+
+    easyfirst_keys = ['WORD', 'MASK', 'LENGTH', 'POS', 'CHAR', 'HEAD', 'TYPE']
+    all_keys = easyfirst_keys + ['RECOMP', 'GEN_HEAD']
+    batch_by_layer = {i: {key: [] for key in all_keys} for i in range(n_recomp+1)}
+
+    # for every sentence
+    for i in range(len(lengths)):
+        new_order = np.append(np.arange(1,batch_length-1), np.ones(n_recomp) * RECOMP).astype(int)
+        np.random.shuffle(new_order)
+        new_order = np.append(new_order,EOS)
+        n_step = 0
+        generated_heads = np.zeros([1,batch_length], dtype=int)
+        # the input generated head list
+        generated_heads_list = []
+        # whether to recompute at this step
+        recomp_list = []
+        while n_step < len(new_order):
+            next_step = new_order[n_step]
+            if next_step == RECOMP:
+                generated_heads_list.append(np.copy(generated_heads))
+                recomp_list.append(DO_RECOMP)
+                prev_layers = generated_heads_list[-1]
+                # add a new layer
+                generated_heads = np.concatenate([prev_layers,np.zeros([1,batch_length], dtype=int)], axis=0)
+            elif next_step == EOS:
+                generated_heads_list.append(np.copy(generated_heads))
+                recomp_list.append(DO_EOS)
+            else:
+                generated_heads_list.append(np.copy(generated_heads))
+                recomp_list.append(NO_RECOMP)
+                # add one new head to the top layer of generated heads
+                generated_heads[-1,next_step] = 1
+            n_step += 1
+        
+        #print ("new_order", new_order)
+        """
+        print ('generated_heads_list:')
+        for h in generated_heads_list:
+            print (h)
+        print ('recomp_list:', recomp_list)"""
+        
+        for gen_heads, recomp in zip(generated_heads_list, recomp_list):
+            n_layers = len(gen_heads) - 1
+            for key in easyfirst_keys:
+                batch_by_layer[n_layers][key].append(batch[key][i])
+            batch_by_layer[n_layers]['RECOMP'].append(recomp)
+            batch_by_layer[n_layers]['GEN_HEAD'].append(gen_heads)
+    for n_layers in batch_by_layer.keys():
+        for key in batch_by_layer[n_layers].keys():
+            batch_by_layer[n_layers][key] = np.stack(batch_by_layer[n_layers][key])
+        batch_by_layer[n_layers]['GEN_HEAD'] = np.transpose(batch_by_layer[n_layers]['GEN_HEAD'], (1,0,2))
+    #print (batch_by_layer)
+    return batch_by_layer
+        
 
 def iterate_data(data, batch_size, bucketed=False, unk_replace=0., shuffle=False):
     if bucketed:
         return iterate_bucketed_batch(data, batch_size, unk_replace==unk_replace, shuffle=shuffle)
     else:
         return iterate_batch(data, batch_size, unk_replace==unk_replace, shuffle=shuffle)
+
+def iterate_data_and_sample(data, batch_size, bucketed=False, unk_replace=0., shuffle=False, max_layers=6):
+    return iterate_bucketed_batch_and_sample(data, batch_size, unk_replace==unk_replace, 
+            shuffle=shuffle, max_layers=max_layers)
+
+if __name__ == '__main__':
+    easyfirst_keys = ['WORD', 'MASK', 'LENGTH', 'POS', 'CHAR', 'HEAD', 'TYPE']
+    batch = {'WORD':[[0,1,2,3,4,5],[0,6,7,8,0,0]],'MASK':[[1,1,1,1,1,0],[1,1,1,1,1,1]],
+             'POS':[[0,1,2,3,4,5],[0,6,7,8,0,0]], 'LENGTH':np.array([6,5]),
+             'CHAR':[[0,1,2,3,4,5],[0,6,7,8,0,0]],'HEAD':[[0,3,1,0,3,5],[0,6,7,8,0,0]],'TYPE':[[0,1,2,3,4,5],[0,6,7,8,0,0]]}
+    lengths = batch['LENGTH']
+    sample_generate_order(batch, lengths, n_recomp=3)
--- neuronlp2/nn/modules.py
+++ neuronlp2/nn/modules.py
@@ -9,6 +9,49 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 
+class BiAffine_v2(nn.Module):
+
+    def __init__(self, n_in, n_out=1, bias_x=True, bias_y=True):
+        super(Biaffine, self).__init__()
+
+        self.n_in = n_in
+        self.n_out = n_out
+        self.bias_x = bias_x
+        self.bias_y = bias_y
+        self.weight = nn.Parameter(torch.Tensor(n_out,
+                                                n_in + bias_x,
+                                                n_in + bias_y))
+        self.reset_parameters()
+
+    def extra_repr(self):
+        s = f"n_in={self.n_in}, n_out={self.n_out}"
+        if self.bias_x:
+            s += f", bias_x={self.bias_x}"
+        if self.bias_y:
+            s += f", bias_y={self.bias_y}"
+
+        return s
+
+    def reset_parameters(self):
+        #nn.init.zeros_(self.weight)
+        nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, x, y):
+        if self.bias_x:
+            x = torch.cat([x, x.new_ones(x.shape[:-1]).unsqueeze(-1)], -1)
+        if self.bias_y:
+            y = torch.cat([y, y.new_ones(y.shape[:-1]).unsqueeze(-1)], -1)
+        # [batch_size, 1, seq_len, d]
+        x = x.unsqueeze(1)
+        # [batch_size, 1, seq_len, d]
+        y = y.unsqueeze(1)
+        # [batch_size, n_out, seq_len, seq_len]
+        s = x @ self.weight @ y.transpose(-1, -2)
+        # remove dim 1 if n_out == 1
+        s = s.squeeze(1)
+
+        return s
+
 
 class BiLinear(nn.Module):
     """
