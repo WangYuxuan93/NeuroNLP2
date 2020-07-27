89ebc7ca2263bee9fda9e831232d2c3cc80e9358
HITalexwang <hitalexwang@gmail.com>
2020-07-27 02:33:19 -0700

    update build_embed_nn, add n_threads

--- adversary/scripts/build_embed_nn.py
+++ adversary/scripts/build_embed_nn.py
@@ -8,6 +8,7 @@ from collections import OrderedDict, defaultdict
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import sparse
 import torch
+import multiprocessing
 
 def load_embedding_dict(path, skip_first=True, normalize_digits=False):
 
@@ -48,8 +49,10 @@ def load_embedding_dict(path, skip_first=True, normalize_digits=False):
 	return embedding, embedd_dim, word2id
 
 class NN(object):
-	def __init__(self, embed, k=101, pre_compute=True):
+	def __init__(self, embed, k=101, pre_compute=True, n_thread=4):
+		self.n_thread = n_thread
 		self.embed = embed
+		self.num = len(self.embed)
 		if k > len(embed):
 			print ("Total embed number {} > k ({})".format(len(embed), k))
 			self.k = len(embed)
@@ -81,40 +84,68 @@ class NN(object):
 		except KeyError:
 			e1 = self.embed[a]
 			e2 = self.embed[b]
-			#e1 = torch.tensor(e1)
-			#e2 = torch.tensor(e2)
-			#cos_sim = torch.nn.CosineSimilarity(dim=0)(e1, e2).numpy()
-			cos_sim = self.one_cos_sim(e1, e2)
-			self.cos_sim_mat[a][b] = cos_sim
+			e1 = torch.tensor(e1)
+			e2 = torch.tensor(e2)
+			cos_sim = torch.nn.CosineSimilarity(dim=0)(e1, e2).numpy()
+			#cos_sim = self.one_cos_sim(e1, e2)
+			#self.cos_sim_mat[a][b] = cos_sim
 		return cos_sim
 
+	def compute_nn_single(self, id):
+		top_nn = []
+		top_sim = []
+		#print ("runing ", id)
+		for j in range(self.num):
+			sim = self.cos_sim(id, j)
+			if len(top_nn) < self.k:
+				top_nn.append(j)
+				top_sim.append(sim)
+			else:
+				if sim > sims.min():
+					min_id = sims.argmin()
+					sims[min_id] = sim
+					top_nn[min_id] = j
+			if len(top_nn) == self.k:
+				sims = np.array(top_sim)
+
+		order = (-sims).argsort()
+		nn_single = np.array(top_nn)[order]
+		#print (id, nn_single)
+
+		return id, nn_single, sims, top_nn
+
+	def update(self, result):
+		#print ("result", result)
+		i, nn_single, sims, top_nn = result
+		self.nn_list.append(nn_single)
+
+		for sim, j in zip(sims, top_nn):
+			a, b = min(i,j), max(i,j)
+			self.cos_sim_mat[a][b] = sim
+
 	def compute_nn(self, log_every=1000):
-		num = len(self.embed)
-		nn_list = []
-		for i in range(num):
+		pool = multiprocessing.Pool(processes=self.n_thread)
+		self.nn_list = []
+		results = []
+		for i in range(self.num):
 			if i % log_every == 0:
 				print (i,"...", end="")
 				sys.stdout.flush()
-			top_nn = []
-			top_sim = []
-			for j in range(num):
-				sim = self.cos_sim(i, j)
-				if len(top_nn) < self.k:
-					top_nn.append(j)
-					top_sim.append(sim)
-				else:
-					if sim > sims.min():
-						min_id = sims.argmin()
-						sims[min_id] = sim
-						top_nn[min_id] = j
-				if len(top_nn) == self.k:
-					sims = np.array(top_sim)
-			order = (-sims).argsort()
-			ord_nn = []
-			for m in order:
-				ord_nn.append(top_nn[m])
-			nn_list.append(np.array(ord_nn))
-		self.nn = np.stack(nn_list, axis=0)
+			res = pool.apply_async(self.compute_nn_single, args=(i,))
+			results.append(res)
+			"""
+			nn_single, sims, top_nn = self.compute_nn_single(i)
+			nn_list.append(nn_single)
+			for sim, j in zip(sims, top_nn):
+				a, b = min(i,j), max(i,j)
+				self.cos_sim_mat[a][b] = sim
+			"""
+		pool.close()
+		pool.join()
+		for r in results:
+			self.update(r.get())
+
+		self.nn = np.stack(self.nn_list, axis=0)
 		print ()
 		#print (self.cos_sim_mat)
 		return self.nn
@@ -143,6 +174,7 @@ parser.add_argument("embedding", type=str, help="input embedding file")
 parser.add_argument("out_dir", type=str, help="output path")
 #parser.add_argument("--out_vocab", type=str, default="vocab.json", help="output path for vocab")
 parser.add_argument("--k", type=int, default=101, help="save top k NN")
+parser.add_argument("--n_thread", type=int, default=10, help="number of threads")
 parser.add_argument("--nn", action="store_true", help="whether to compute NN")
 parser.add_argument("--pre", action="store_true", help="whether to pre-compute NN matrix")
 parser.add_argument("--log_every", type=int, default=1000, help="print log every")
@@ -170,12 +202,18 @@ np.save(emb_path, embed)
 #print (embed)
 
 if args.nn:
-	nn_computer = NN(embed, k=args.k, pre_compute=args.pre)
+	nn_computer = NN(embed, k=args.k, pre_compute=args.pre, n_thread=args.n_thread)
 	nn = nn_computer.compute_nn(log_every=args.log_every)
 	nn_path = os.path.join(args.out_dir, 'nn')
 	np.save(nn_path, nn)
 	#print (nn)
 
+	trim_cos_sim_mat = nn_computer.cos_sim_mat
+	trim_cos_path = os.path.join(args.out_dir, 'cos_sim.p')
+	with open(trim_cos_path, 'wb') as f:
+		pickle.dump(trim_cos_sim_mat, f)
+
+	exit()
 	trim_cos_sim_mat, all_cos_sim_mat = nn_computer.get_cos_sim(k=args.k)
 	trim_cos_path = os.path.join(args.out_dir, 'cos_sim.p')
 	with open(trim_cos_path, 'wb') as f:
