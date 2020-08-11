685ab7bbe23580edb12cdc0667d459eabfe5c58d
HITalexwang <hitalexwang@gmail.com>
2020-08-11 16:09:36 -0700

    add dp_attack

---
+++ adversary/scripts/dp_attack.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python -u zheng/dpattack.py --mode black \
+ --min_word_cos_sim 0.9 --min_sent_cos_sim 0.9 --filters 'embedding' --generators 'synonym:sememe:embedding' \
+ --batch_size 32 \
+ --noscreen \
+ --punctuation '.' '``' "''" ':' ',' \
+ --pretrained_lm none \
+ --format ud \
+ --test "../experiments/data/ptb_two_auto_orig.conll" \
+ --lan_test en \
+ --model_path "../experiments/models/parsing/robust" \
+ --output_filename "ptb_two_auto.pred.conll" \
+ --adv_filename "ptb_two_auto.adv.conll" \
+ --vocab "data/vocab.json" --cand "data/word_candidates_sense.json" \
+ --syn "data/two_synonyms.json" \
+ --adv_rel_ratio 0 --adv_fluency_ratio 0 --max_mod_percent 0.2 \
+ --max_knn_candidates 20 --knn_path "output/embed" --train_vocab "data/vocab.json" \
+ --basic_word_embedding #--cand_cache_path "output/cache.json"
+ #--use_pad
+ #--random_sub_if_no_change 
+ # --mix_datasets
