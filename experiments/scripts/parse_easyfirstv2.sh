d7b4ca0e365eae182ce7ce17bdd2f71d0f2eefe2
jeffrey <zhilinhust@126.com>
2021-01-07 18:35:09 +0800

    cache

--- experiments/scripts/parse_easyfirstv2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python -u easyfirst_parsing_v2.py --mode parse --batch_size 32 \
- --noscreen --get_head_by_layer \
- --punctuation '.' '``' "''" ':' ',' \
- --test "data/ptb_two_auto.conll" \
- --model_path "models/parsing/easyfirst_v2/" \
- --output_filename "models/parsing/easyfirst_v2/pred.conll" #--random_recomp --recomp_prob 0.4
